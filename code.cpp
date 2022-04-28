4ab2a42f22523634f34e2943549896828243563a
Zain Ul Abidin <104536967+Zain970946@users.noreply.github.com>
2022-04-28 23:51:21 +0500

    Simple OpenMp Program

--- code.cpp
+++ code.cpp
@@ -2,10 +2,13 @@
 #include<omp.h>
 int main()
 {
+//     Using open mp library to make many threads
     std::cout<<"Welcome to mutithreading with omp "<<std::endl;
-
+    
+//     Initializing number of threads
     omp_set_num_threads(3);
-
+   
+//     Parallel region
     #pragma omp parallel
     {
         std::cout<<"Hello from thread number : "<<omp_get_thread_num()<<std::endl;
